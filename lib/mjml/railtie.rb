dbb18740bf8725701135d1f8b8ec063c21687ae1
Jim Benton <jim@autonomousmachine.com>
2019-07-22 21:52:26 -0500

    Automatically configure mjml_path under Rails.

--- lib/mjml/railtie.rb
+++ lib/mjml/railtie.rb
@@ -7,5 +7,9 @@ module Mjml
       ActionView::Template.register_template_handler :mjml, Mjml::Handler.new
       Mime::Type.register_alias "text/html", :mjml
     end
+
+    initializer "mjml-rails.configure_mjml_path", after: "load_config_initializers" do
+      Mjml.configure_mjml_path!
+    end
   end
 end
